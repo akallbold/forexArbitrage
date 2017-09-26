c3916f10f80cf3d60e61a3c224f4d4a7ec4d041a
akallbold <a.kallenbornbolden@gmail.com>
2017-09-26 15:40:58 -0400

    javascript comments

---
+++ forexArbitrage.js
@@ -0,0 +1,10 @@
+//pull in data from internet
+
+//convert rates to linear values by logging values
+
+//run several permutations of exchange
+
+//if arbitrage opportunity available create trade
+
+//print delta from trade
+//print total profits
